8f5c0dfd0dd01be1880a8013aa37eef134ee1a60
Mazinger0501 <j2u3n4@gmail.com>
2022-11-30 11:42:48 +0900

    Commit Message

---
+++ login_test/amplify-react-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyreactappb91bd759": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}

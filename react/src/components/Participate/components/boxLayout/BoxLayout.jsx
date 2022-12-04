20bc79c184e75fccf46ac52ba2ae751b7881915b
jaewook5267 <jawook1212@naver.com>
2022-12-04 19:29:39 +0900

    participate ok

--- react/src/components/Participate/components/boxLayout/BoxLayout.jsx
+++ react/src/components/Participate/components/boxLayout/BoxLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './boxLayout.module.css';
-export default function BoxLayout({ children, props }) {
+export default function BoxLayout({ children }) {
   return (
     <div className={styles.container}>
       {children}

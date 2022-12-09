01b2adbcf14bebd54959bd5578cc6ed2ce4b4962
JY <ljykevin3882@gmail.com>
2022-12-09 18:04:56 +0900

    participate error complete -3

--- react/src/components/Participate/ParticipateComponent.jsx
+++ react/src/components/Participate/ParticipateComponent.jsx
@@ -6,13 +6,15 @@ import styles from './ParticipateComponent.module.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import SignIn from './Chat/SignIn';
 import { auth } from './firebase';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom'; // 12/09 작성 - useNavigate import 추가
 import getData from '../../service/getData';
 
 
-export default function ParticipateComponent() { 
+export default function ParticipateComponent() {
 
-  
+    // 12/09 작성 - 이전 목록으로 돌아가기 
+    const navigate = useNavigate();
+
     const [uid, setUid] = useState("");
     let param = useParams();
     console.log(param['*']);
@@ -20,7 +22,7 @@ export default function ParticipateComponent() {
     // setUid(meetUUID);
     // console.log("look at", uid);
 
-  
+
     const [user] = useAuthState(auth)
     return (
         <div className={styles.container}>
@@ -39,6 +41,12 @@ export default function ParticipateComponent() {
                 </div>
             </div>
 
+            {/* 12/09 작성 - 이전으로 돌아가는 버튼 추가 */}
+            <div className={styles.btns_participate}>
+                <button className={styles.btn_backToDetail} onClick={() => {
+                    navigate(-1);
+                }}>이전으로</button>
+            </div>
         </div>
     );
 };

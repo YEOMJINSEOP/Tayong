54df05c9215446db632c5b0facfa9441f73c1b84
yeomjinseop <yeomjinseop@gmail.com>
2022-12-19 11:09:08 +0900

    remove unnecessary Participate Component

--- react/src/components/Participate/components/boxLayout/BoxLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './boxLayout.module.css';
-export default function BoxLayout({ children }) {
-  return (
-    <div className={styles.container}>
-      {children}
-    </div>
-  )
-}

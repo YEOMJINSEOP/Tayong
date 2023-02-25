8139c16081703f8a544255fe1ccbc1d84fe7dc72
yeomjinseop <yeomjinseop@gmail.com>
2023-02-25 16:14:58 +0900

    change port to 8081

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
+
 const io = new Server({httpServer,
   cors:{
     origin: '*'
@@ -18,5 +19,5 @@ io.on('connection', (socket) => {
   })
 })
 
-io.listen(8080);
+io.listen(8081);
 

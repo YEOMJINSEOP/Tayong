a6fb58b0d352b479a8a812538d28ba1fbf6388f3
yeomjinseop <yeomjinseop@gmail.com>
2023-03-06 16:42:22 +0900

    remove footer content

--- react/src/components/footer/footer.jsx
+++ react/src/components/footer/footer.jsx
@@ -4,8 +4,6 @@ import styles from './footer.module.css';
 function Footer(props) {
   return (
      <footer className={styles.footer}>
-      <p className={styles.contract}>이용약관</p>
-      <span className={styles.divider}>  |  </span>
       <p className={styles.name}>@Tayong 2023</p>
      </footer> 
   );

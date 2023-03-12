e9c4aef5b2ebdb37637cd5b93ea7bcedfc9737d1
yeomjinseop <yeomjinseop@gmail.com>
2023-03-12 13:27:36 +0900

    form css fix on mobile

--- react/src/components/chat/chat.jsx
+++ react/src/components/chat/chat.jsx
@@ -48,8 +48,6 @@ function Chat({meetId}) {
     if(socket){
       socket.on('message', (msg) => {
         setChat([...chat, msg]);
-        console.log(msg.userMail);
-        console.log(userMail);
         if(msg.userMail !== userMail){
           new Notification(msg.message);
         }

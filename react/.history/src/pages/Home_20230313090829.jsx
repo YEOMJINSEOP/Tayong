860ad0ec670a1a8e780006554114a3b69e8e0943
yeomjinseop <yeomjinseop@gmail.com>
2023-03-16 14:40:51 +0900

    fix meetDetail css Error

--- react/.history/src/pages/Home_20230313090829.jsx
+++ react/.history/src/pages/Home_20230313090829.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import LocationSelector from '../components/locationSelector/locationSelector';
 function Home(props) {
+  // useEffect(() => {
+  //   Notification.requestPermission();
+  // }, [])
   return (
       <LocationSelector/>
   );
